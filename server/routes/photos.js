1be23e7c5751141620a6e6fc3a3143bbaa132063
Pratik <pratik.patil22307@gmail.com>
2026-06-10 01:52:45 +0530

    debug upload controller

--- server/routes/photos.js
+++ server/routes/photos.js
@@ -6,7 +6,21 @@ const upload = require('../middleware/upload');
 
 router.use(protect);
 router.get('/', getPhotos);
-router.post('/', upload.single('image'), uploadPhoto);
+router.post('/', (req, res, next) => {
+  upload.single('image')(req, res, function (err) {
+
+    if (err) {
+      console.error('MULTER ERROR:', err);
+
+      return res.status(500).json({
+        message: 'Multer failed',
+        error: err.message
+      });
+    }
+
+    next();
+  });
+}, uploadPhoto);
 router.delete('/:id', deletePhoto);
 
 module.exports = router;
